f088bf5197d664246cc2a35e8c29925911866c7d
ammojest <63594225+ammojest@users.noreply.github.com>
2022-03-17 07:31:39 +0000

    homepage update

--- components/holding-screen.js
+++ components/holding-screen.js
@@ -6,7 +6,8 @@ const Holding = () => {
         <div className="block">
             <div className="relative bg-[url('/sykes_image_30-medium.jpg')] bg-cover bg-bottom shadow-dark-60 h-[60vh]" id="bg-image">
                 <div className="flex flex-col justify-center items-center h-full px-3 text-white" id="header-text">
-                    <h1 className="brightness-100 z-20 text-xl uppercase text-center tracking-widest leading-10 my-3 font-bold" id="header-text">Ch-ch-ch-changes</h1>
+                    <h1 className="brightness-100 z-20 text-xl uppercase text-center tracking-widest leading-10 my-3 font-bold" id="header-text">CUSTOMER ANNOUNCEMENT</h1>
+                    <p className="my-2 py-3 md:w-1/2 brightness-100 text-center font-thin text-white">We&#39;ve made some changes to our website and the way our customers transact with us. This site is now dedicated to providing information and the latest news about our industry, brands and partners.</p>
                     <p className="my-2 py-3 md:w-1/2 brightness-100 text-center font-thin text-white">We made some changes to our website, we have moved our Store to <a className="font-bold" href="https://seafooodbysykes.com" rel="noreferrer" target="_blank">seafoodbysykes.com</a> or you can always call our sales staff on <a href="tel:03334441862"className="font-bold">0333 444 1862</a> or email <a href="mailto:customer.service@sykesseafood.com" className="font-bold">customer.service@sykesseafood.com</a> to help with your order. This site is now dedicated to giving you the latest news about Sykes and our family of businesses</p>
                 </div>
             </div>
