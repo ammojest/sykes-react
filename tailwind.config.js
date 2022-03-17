c2e91a2c55179272e9666693596b6ac17aa53bcb
ammojest <63594225+ammojest@users.noreply.github.com>
2022-03-17 08:55:24 +0000

    change body text colour

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ module.exports = {
       },
       colors: {
         brand: "#233681",
-        copy: "#a6acb0",
+        copy: "##666666",
         footer: "#9f9f9f",
         Whitehover: "#ffffff59",
     },
